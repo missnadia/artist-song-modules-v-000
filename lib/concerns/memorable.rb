576bfc52ae5a64339a04117d3d2b280d8e2d8bf1
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-30 23:53:10 +0000

    automatically backed up by learn

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,17 @@
+module Memorable
+  def self.reset_all
+    all.clear
+  end
+
+  def self.count
+    @@artists.count
+  end
+
+  def self.reset_all
+    @@songs.clear
+  end
+
+  def self.count
+    all.count
+  end
+end
