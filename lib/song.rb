6e53aae1455887993afc88931355559a38bccb58
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-30 23:55:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,8 @@ class Song
 
   @@songs = []
 
+  extend Memorable::ClassMethods
+
   def initialize
     @@songs << self
   end
@@ -18,16 +20,8 @@ class Song
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
-
   def to_param
     name.downcase.gsub(' ', '-')
-  end
-
-  def self.count
-    self.all.count
   end
 
   def self.all
