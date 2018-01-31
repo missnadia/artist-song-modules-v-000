ca577abca0afda27ad7a366af4aaba31e0c014df
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-31 00:00:47 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,9 @@
 module Findable
+  def find_by_name(name)
+    @@artists.detect{|a| a.name == name}
+  end
 
-
-  def self.find_by_name(name)
+  def find_by_name(name)
     @@songs.detect{|a| a.name == name}
   end
 end
